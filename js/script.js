2cc637825f976190394051ea972388737183b189
Harsh Pandya <pandyaharsh43@gmail.com>
2019-03-20 19:16:51 -0500

    Landing page ready

---
+++ js/script.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+    $('#slides').superslides({
+        animation:'fade',
+        play:5000,
+        pagination:false
+    });
+
+    var typed = new Typed(".typed", {
+        strings:["Software Engineer", "MS in Computer Science"],
+        typeSpeed: 100,
+        loop:true,
+        startDelay:1000,
+        showCursor:false
+    });
+});
+
